b1964bf7383844392526c84cd8064cfa1108760f
Dmitriy_Selutin <pblpra8@gmail.com>
2025-04-17 13:10:35 +0300

    edit models.py

--- catalog/models.py
+++ catalog/models.py
@@ -17,6 +17,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    objects = None
     name = models.CharField(
         max_length=150, verbose_name="Название", help_text="Введите название"
     )
